5153fb10317ff3438b3a941314fc4a81f20d7604
Jamie Shepherd <email@jamie.sh>
2015-06-29 23:10:12 +0100

    Basic structure, sass set up

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,55 @@
 <html>
 <head>
     <title>Welcome</title>
+
+    <link media="all" type="text/css" rel="stylesheet" href="{{ elixir("css/app.css") }}">
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
 </head>
 <body>
-    Welcome
+    <section class="cover">
+    <div class="gradient">
+        <div class="details">
+            <span class="tag">Film</span>
+            <h1>Inception</h1>
+            <h6><strong>2010</strong> directed by <strong>Christopher Nolan</strong></h6>
+            <p>A thief who steals corporate secrets through use of dream-sharing technology is given the inverse task of planting an idea into the mind of a CEO.</p>
+        </div>
+    </div>
+    </section>
+    <section class="information">
+        <div class="quick-nav">
+            PAUL MESSENGER
+        </div>
+        <div class="user-review">
+            <h2>What did you think about Inception?</h2>
+            <input type="text" placeholder="Enter your review now...">
+        </div>
+        <div class="feed">
+            <div class="review">
+                <p>“Really fun film, Chris Pratt was great, and it really exceeded my expectations. Funny, action packed, and thoroughly enjoyable. Looking forward to a sequel!”</p>
+                <div class="details">
+                    PAUL MESSENGER
+                </div>
+            </div>
+            <div class="review">
+                <p>“Really fun film, Chris Pratt was great, and it really exceeded my expectations. Funny, action packed, and thoroughly enjoyable. Looking forward to a sequel!”</p>
+                <div class="details">
+                    PAUL MESSENGER
+                </div>
+            </div>
+            <div class="review">
+                <p>“Really fun film, Chris Pratt was great, and it really exceeded my expectations. Funny, action packed, and thoroughly enjoyable. Looking forward to a sequel!”</p>
+                <div class="details">
+                    PAUL MESSENGER
+                </div>
+            </div>
+            <div class="review">
+                <p>“Really fun film, Chris Pratt was great, and it really exceeded my expectations. Funny, action packed, and thoroughly enjoyable. Looking forward to a sequel!”</p>
+                <div class="details">
+                    PAUL MESSENGER
+                </div>
+            </div>
+        </div>
+    </section>
 </body>
 </html>
