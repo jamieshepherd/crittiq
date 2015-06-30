84465870ed09df951e737d71d298f3d28a4ae3c5
Jamie Shepherd <email@jamie.sh>
2015-06-30 09:01:48 +0100

    Some tidying, figuring out numbered circle styles

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,6 +4,7 @@
     <title>Welcome</title>
 
     <link media="all" type="text/css" rel="stylesheet" href="{{ elixir("css/app.css") }}">
+    <link media="all" type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
 </head>
 <body>
@@ -18,8 +19,13 @@
     </div>
     </section>
     <section class="information">
-        <div class="quick-nav">
-            PAUL MESSENGER
+        <div class="mini-nav">
+            <div class="user-info">
+                <span class="user-avatar">Q</span>JIM BEAN><span class="level">7</span><span class="points">+10,000 points</span>
+            </div>
+            <div class="search">
+                SEARCH
+            </div>
         </div>
         <div class="user-review">
             <h2>What did you think about Inception?</h2>
