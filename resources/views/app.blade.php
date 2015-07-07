36652ef5fc7412441c4e22d15110247633846c24
Jamie Shepherd <email@jamie.sh>
2015-07-07 16:10:33 +0100

    Added some meta tags and tidied header

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,10 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Welcome</title>
+    <!-- Title -->
+    <title>Inception (2010) - crittiq</title>
 
+    <!-- Meta tags -->
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="description" content="Crittiq is a micro review site for TV, Film, and Games">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Styles -->
     <link media="all" type="text/css" rel="stylesheet" href="{{ elixir("css/app.css") }}">
     <link media="all" type="text/css" rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+
+    <!-- Scripts -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
     <script src="/js/vendor/vue-0.12.6.js"></script>
     <script src="/js/vendor/jquery-2.1.4.js"></script>
