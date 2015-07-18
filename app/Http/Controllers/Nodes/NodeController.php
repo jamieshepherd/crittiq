e98753593734834c9fc9e53cba44f0a9fbcd1d14
Jamie Shepherd <email@jamie.sh>
2015-07-18 10:42:25 +0100

    Fixed review average and input styling

--- app/Http/Controllers/Nodes/NodeController.php
+++ app/Http/Controllers/Nodes/NodeController.php
@@ -56,7 +56,7 @@ class NodeController extends Controller
 
         //dd($node->_id);
 
-        $avg = number_format(round(Review::where('node', $node->_id)->avg('score'),1),1);
+        $avg = number_format(round(Review::where('node.reference', $node->_id)->avg('score'),1),1);
         if($avg == "0.0") {
             $avg = "&mdash;";
         }
