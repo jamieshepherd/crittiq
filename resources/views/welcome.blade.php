7e331a6f36019eb7ca93da697a8b04131d92cd23
Jamie Shepherd <email@jamie.sh>
2015-08-18 22:42:54 +0100

    Made a start on homepage previews

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,10 +13,48 @@
             <span><a v-on="click:showLogin">Login</a> or <a href='/auth/register'>join crittiq</a> now to start making micro reviews about films you love.</span>
         </div>
     </header>
-    <div class="">
-
+    <div class="previews">
+        <div class="preview">
+            <span class='category'>Films</span>
+            <img src="/images/features/preview-child44.jpg">
+            <div class="content">
+                <div class="reaction-bar">
+                    <div class="part negative"></div>
+                    <div class="part mixed"></div>
+                    <div class="part positive"></div>
+                </div>
+                <h3>Child 44</h3>
+                <p><strong>2010</strong> directed by <strong>Christopher Nolan</strong></p>
+            </div>
+        </div>
+        <div class="preview">
+            <span class='category'>TV</span>
+            <img src="/images/features/preview-child44.jpg">
+            <div class="content">
+                <div class="reaction-bar">
+                    <div class="part negative"></div>
+                    <div class="part mixed"></div>
+                    <div class="part positive"></div>
+                </div>
+                <h3>Child 44</h3>
+                <p><strong>2010</strong> directed by <strong>Christopher Nolan</strong></p>
+            </div>
+        </div>
+        <div class="preview">
+            <span class='category'>Games</span>
+            <img src="/images/features/preview-child44.jpg">
+            <div class="content">
+                <div class="reaction-bar">
+                    <div class="part negative"></div>
+                    <div class="part mixed"></div>
+                    <div class="part positive"></div>
+                </div>
+                <h3>Child 44</h3>
+                <p><strong>2010</strong> directed by <strong>Christopher Nolan</strong></p>
+            </div>
+        </div>
     </div>
-
+    <br><br><br><br><br><br><br><br><br><br><br><br>
 
 
     <script src="/js/app/components/search.js"></script>
